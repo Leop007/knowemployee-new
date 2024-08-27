bd0c28229028e80c4d210ad285c7902c36da1c5d
Leonard Perry <perry.leonard@gmail.com>
2024-08-27 17:41:21 +0300

    changes for docker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,13 @@
 # gunicorn.conf.py
 # Non logging stuff
 ### Create directory and files in it and give it permission of user that is running the app as well, add log files to logrotatae ###
-bind = "0.0.0.0:80"
+bind = "127.0.0.1:8005"
 workers = 3
 # Access log - records incoming HTTP requests
-accesslog = "/var/log/gunicorn/gunicorn.access.log"
+accesslog = "gunicorn/gunicorn.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "/var/log/gunicorn/gunicorn.error.log"
+errorlog = "gunicorn/gunicorn.error.log"
 # Whether to send Django output to the error log 
 capture_output = True
 # How verbose the Gunicorn error logs should be 
-loglevel = "info"
+loglevel = "debug"
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from knowemployee import app, run_analytics
+from server import app, run_analytics
 
 if __name__ == '__main__':
     Thread(target=run_analytics).start()
