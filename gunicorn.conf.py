8d850c8d19fcf07dd16cae2d6f79dfeb870019b6
Leonard Perry <perry.leonard@gmail.com>
2024-08-27 17:55:42 +0300

    changes for docker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,12 @@
 # gunicorn.conf.py
 # Non logging stuff
 ### Create directory and files in it and give it permission of user that is running the app as well, add log files to logrotatae ###
-bind = "127.0.0.1:8005"
+bind = "0.0.0.0:8005"
 workers = 3
 # Access log - records incoming HTTP requests
-accesslog = "gunicorn/gunicorn.access.log"
+accesslog = "gunicorn/access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "gunicorn/gunicorn.error.log"
+errorlog = "gunicorn/error.log"
 # Whether to send Django output to the error log 
 capture_output = True
 # How verbose the Gunicorn error logs should be 
